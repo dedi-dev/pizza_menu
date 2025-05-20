2a7becf18e4849bcbeed7ecc71da499480047043
dedi <dedi.dedi@jatis.com>
2025-05-20 09:37:27 +0700

    add package gh-page

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
-    tailwindcss(),
+    tailwindcss()
   ],
-  base: "/pizza-menu"
+  base: "/pizza_menu/"
 })
